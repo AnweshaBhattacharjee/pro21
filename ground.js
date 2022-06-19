f7bf5e551946f165880af75b5c8b1ae197c9e0c9
AnweshaBhattacharjee <106428442+AnweshaBhattacharjee@users.noreply.github.com>
2022-06-19 12:01:18 +0530

    Add files via upload

---
+++ ground.js
@@ -0,0 +1,20 @@
+class Ground {
+    constructor(x,y,w,h){
+        let option ={
+            isStatic:true
+        };
+        this.body = Bodies.rectangle(x,y,w,h,option)
+        World.add(world,this.body)
+        this.w = w
+        this.h = h
+
+    }
+    display(){
+        var pos = this.body.position
+        rect(pos.x,pos.y,this.w,this.h);
+
+       
+    }
+}
+
+
